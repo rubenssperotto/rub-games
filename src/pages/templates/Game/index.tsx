17212a68acea938ee64e699c6f40f1d607b3244c
Rubens Speroto <rubens_94@hotmail.com>
2022-06-27 19:47:43 -0300

    add page wishlist and empty page

--- src/pages/templates/Game/index.tsx
+++ src/pages/templates/Game/index.tsx
@@ -5,6 +5,7 @@ import GameDetails, { GameDetailsProps } from 'components/GameDetails'
 import { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 import Showcase from 'components/Showcase'
+import { Divider } from 'components/Divider'
 
 import * as S from './styles'
 import TextContent from 'components/TextContent'
@@ -45,6 +46,7 @@ const Game = ({
       </S.SectionDescription>
       <S.SectionGameDetails>
         <GameDetails {...details} />
+        <Divider />
       </S.SectionGameDetails>
       <Showcase
         title="Upcoming"
